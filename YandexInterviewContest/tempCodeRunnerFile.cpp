1c0571cf206b3714449d1674772f4de06deb1bf9
xCthulhuFtagn <mkolodyazhny@gmail.com>
2021-09-23 11:06:34 +0300

    aaa

--- YandexInterviewContest/tempCodeRunnerFile.cpp
+++ YandexInterviewContest/tempCodeRunnerFile.cpp
@@ -1,16 +1,5 @@
 #include <iostream>
+#include <vector>
  using namespace std;
 
- int main(){ 
-     size_t N;
-     int32_t ass, prev_ass = INT32_MIN;
-     cin >> N;
-     for(size_t i =0; i< N; ++i){
-         cin >> ass;
-         if(prev_ass != ass){
-             cout << ass << endl; //ass
-         }
-         prev_ass = ass;
-         //ass
-     }
- }
+ int main(){
